c515f4a5bee4e4176568d5602edbe0b8600b24c7
vltmn <melkerveltman@gmail.com>
2019-07-21 19:02:12 +0200

    react: remove logging in wizard

--- development/frontend-react/src/containers/Wizard.tsx
+++ development/frontend-react/src/containers/Wizard.tsx
@@ -18,15 +18,12 @@ export interface WizardProps {
     steps: Step[];
 }
 
-// TODO use hooks instead
-
 export const Wizard: React.FunctionComponent<WizardProps> = props => {
     const [currentInput, setCurrentInput] = useState<any>({});
     const [currentStepIndex, setCurrentStepIndex] = useState(0);
     const [loading, setLoading] = useState(false);
 
     const nextStep = (output: any) => {
-        console.log(output, props.steps);
         setLoading(true);
         setCurrentInput(output);
         setCurrentStepIndex(currentStepIndex + 1);
@@ -34,32 +31,8 @@ export const Wizard: React.FunctionComponent<WizardProps> = props => {
     };
 
     const stepComponentInput = currentInput;
-    console.log('RENDERING', stepComponentInput, currentStepIndex, props.steps);
     const StepComponent = props.steps[currentStepIndex].component;
     return (loading ? <span></span> :
         <StepComponent input={stepComponentInput} onCompleted={nextStep} />
     );
 };
-
-export class WizardOld extends React.Component<WizardProps, WizardState> {
-    constructor(props: Readonly<WizardProps>) {
-        super(props);
-        this.state = {
-            currentStepIndex: 0,
-            currentStepInput: null
-        };
-    }
-
-    nextStep = (data: any) => {
-        const newLocal = this;
-        newLocal.setState({
-            currentStepIndex: this.state.currentStepIndex + 1,
-            currentStepInput: data
-        });
-    }
-    render() {
-        const currentStep = this.state.currentStepIndex;
-        const StepComponent = this.props.steps[currentStep].component;
-        return (<StepComponent input={{}} onCompleted={this.nextStep} />);
-    }
-}
