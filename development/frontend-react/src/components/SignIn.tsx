94e5da8efc97eaaba6ad34476835f1eb9267eb40
vltmn <melkerveltman@gmail.com>
2019-07-21 19:02:25 +0200

    react: use form in signin

--- development/frontend-react/src/components/SignIn.tsx
+++ development/frontend-react/src/components/SignIn.tsx
@@ -10,38 +10,45 @@ export const SignInComponent: React.FunctionComponent<SignInComponentProps> = (
 ) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const submitForm = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    props.signIn(username, password);
+  };
+
   return (
     <div className='content'>
-      <p>Please sign in with your account from the CSB homepage.</p>
-      <div className='field'>
-        <input
+      <form action='' onSubmit={submitForm}>
+        <p>Please sign in with your account from the CSB homepage.</p>
+        <div className='field'>
+          <input
+            disabled={props.loading}
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            id='username'
+            type='text'
+            className={`input${props.invalid ? ' is-danger' : ''}`}
+            placeholder='Username'
+          />
+        </div>
+        <div className='field'>
+          <input
+            disabled={props.loading}
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type='password'
+            id='password'
+            className={`input${props.invalid ? ' is-danger' : ''}`}
+            placeholder='Password'
+          />
+        </div>
+        <button
           disabled={props.loading}
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-          id='username'
-          type='text'
-          className={`input${props.invalid ? ' is-danger' : ''}`}
-          placeholder='Username'
-        />
-      </div>
-      <div className='field'>
-        <input
-          disabled={props.loading}
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          type='password'
-          id='password'
-          className={`input${props.invalid ? ' is-danger' : ''}`}
-          placeholder='Password'
-        />
-      </div>
-      <button
-        disabled={props.loading}
-        className={`button is-fullwidth is-info${props.loading ? ' is-loading' : ''}`}
-        onClick={() => props.signIn(username, password)}
-      >
-        Sign in
-      </button>
+          type='submit'
+          className={`button is-fullwidth is-info${props.loading ? ' is-loading' : ''}`}
+        >
+          Sign in
+        </button>
+      </form>
     </div>
   );
 };
